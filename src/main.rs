1bc4637eea2286ebb9cd1b8a29b284751058b0d2
vladdoroniuk <vladyslav.doroniuk@gmail.com>
2023-01-30 19:14:27 +0200

    feat: add --src flag to support custom DSA path

--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,35 @@
 use std::io::Result;
 use std::fs::File;
+use std::path::Path;
 use clap::Parser;
 
 #[derive(Parser)]
 struct Cli {
     dsa_name: String,
+    #[arg(short, long, default_value_t = String::from("src"))]
+    src: String
 }
 
-fn dsa_init(dsa_name: &str) -> Result<()> {
-    std::fs::create_dir(dsa_name)?;
-    std::env::set_current_dir(dsa_name)?;
+fn dsa_init(dsa_name: &str, src: &str) -> Result<()> {
+    let dsa_dir = Path::new(src);
+    std::fs::create_dir_all(&dsa_dir)?;
+    std::env::set_current_dir(&dsa_dir)?;
+    std::fs::create_dir(&dsa_name)?;
+    std::env::set_current_dir(&dsa_name)?;
 
-    let dsa_ts = format!("{}.ts", dsa_name).to_string();
+    let dsa_ts = format!("{}.ts", &dsa_name).to_string();
     File::create(dsa_ts)?;
 
-    let dsa_test_ts = format!("{}.test.ts", dsa_name).to_string();
-    File::create(dsa_test_ts)?;
+    let dsa_test_ts = format!("{}.test.ts", &dsa_name).to_string();
+    File::create(&dsa_test_ts)?;
 
     let dsa_readme = String::from("README.md");
-    File::create(dsa_readme)?;
+    File::create(&dsa_readme)?;
 
     Ok(())
 }
 
 fn main() {
     let args = Cli::parse();
-    dsa_init(&args.dsa_name).unwrap();
+    dsa_init(&args.dsa_name, &args.src).unwrap();
 }
